492fb87d9c659993aaf77bfa1684afd89551b807
ohannuks <otto.hannuksela@fmi.fi>
2013-12-12 12:42:50 +0000

    Added back B_n_bowshock  branch ( I accidentally deleted it)

--- pyCalculations/calculations.py
+++
@@ -1,9 +0,0 @@
-# List of functions and classes that should be imported into the interface
-from cutthrough import cut_through
-from fourier import fourier
-from variable import VariableInfo
-from timeevolution import cell_time_evolution
-from cellids import cellids
-from pitchangle import pitch_angles
-from bn import B_n_bowshock
-import fit
